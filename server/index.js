3c0ac47defc42a0ce9784566e8ea6a3e814954f5
alwaysansh <anshj9y@gmail.com>
2026-02-09 19:48:15 +0530

    updated url in server file

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 // CORS - Simple and Permissive for Development
 // ============================================
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://studdy-buddy-rosy.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
