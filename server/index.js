dbd03d3db4f031cfa79679e198df7dd2d2d2d622
alwaysansh <anshj9y@gmail.com>
2026-04-12 14:53:28 +0530

    url changes for deployment

--- server/index.js
+++ server/index.js
@@ -67,7 +67,7 @@ const app = express();
 // CORS - Simple and Permissive for Development.     https://studdy-buddy-rosy.vercel.app , http://localhost:5173
 // ============================================
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://studdy-buddy-rosy.vercel.app, http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
